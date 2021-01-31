8adf05102f2425e818e3b05a8df18844987ec014
Will Lorand <will@milemarker3.org>
2021-01-31 12:12:54 -0800

    code reviw tweaks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 // mapping libs
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L, { Icon } from 'leaflet';
+// import L, { Icon } from 'leaflet';
 
 // mock-data
-import * as parkData from './mock-data/skateparks.json';
+// import * as parkData from './mock-data/skateparks.json';
 
 // styles
-import 'leaflet/dist/leaflet.css'; // leaflet styles
+// get leaflet styles from node_modules
+import 'leaflet/dist/leaflet.css';
 import './styles/app.css';
 
 function App() {
-  // map vars
+  // map initial vars (state)
   const center = [49.246292, -123.116226]; // Vancouver
-  const initialZoom = 11; // maxZoom = 16 for nat Geo esri tiles (varies by location)
-  const tileURL =
+  const initialZoom = 11; // maxZoom = 16? -- zoom stored in state
+  const tileUrl =
     'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}';
   const tileAttr = 'Tiles &copy; Esri &mdash; National Geographic, ';
 
   return (
     <div className="leaflet-container">
       <MapContainer center={center} zoom={initialZoom}>
-        <TileLayer url={tileURL} attribution={tileAttr} />
+        <TileLayer url={tileUrl} attribution={tileAttr} />
       </MapContainer>
     </div>
   );
